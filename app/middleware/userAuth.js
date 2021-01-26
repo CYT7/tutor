6cea3b2a79a29a3153459b81445e3f1f7f82b1a1
CYT7 <1490930847@qq.com>
2021-01-26 16:56:58 +0800

    添加鉴权中间件

---
+++ app/middleware/userAuth.js
@@ -0,0 +1,54 @@
+/**
+ * @author: Chen yt7
+ * @date: 2020/1/26 4:50 PM
+ */
+'use strict';
+/**
+ * 这是一个前台JWT鉴权中间件，
+ * @param options
+ * @param app
+ * @return {function(...[*]=)}
+ */
+module.exports = (options, app) => {
+  return async function adminAuth(ctx, next) {
+    const param = ctx.request.header.authorization;
+    console.log('==========================================================');
+    // eslint-disable-next-line no-useless-concat
+    console.log('         JWT MiddleWare Start At:  ' + `${ctx.request.url}`);
+    console.log('==========================================================');
+    if (!param) {
+      ctx.body = { code: 404, msg: '没有验证头部' };
+      ctx.status = 201;
+    } else {
+      const arrayParam = param.split(' ');
+      if (arrayParam.length !== 2) {
+        ctx.body = { code: 401, msg: 'Token无效' };
+        ctx.status = 201;
+      } else {
+        let decode;
+        try {
+          const token = param.split(' ')[1];
+          decode = app.jwt.verify(token, options.secret);
+          if (!decode || !decode.id) {
+            ctx.body = { code: 401, msg: 'Token无效' };
+            ctx.status = 201;
+          }
+          if ((Math.round(new Date() / 1000) - decode.exp) > 0) {
+            ctx.body = { code: 401, msg: 'Token过期' };
+            ctx.status = 201;
+          }
+          const user = await ctx.model.User.findOne({ id: decode.name });
+          if (user) {
+            await next();
+          } else {
+            ctx.body = { code: 404, msg: '用户不存在' };
+            ctx.status = 201;
+          }
+        } catch (e) {
+          ctx.body = { code: 401, msg: 'Token无效' };
+          ctx.status = 201;
+        }
+      }
+    }
+  };
+};
--- config/config.default.js
+++ config/config.default.js
@@ -11,11 +11,14 @@ module.exports = appInfo => {
   const config = exports = {};
   // use for cookie sign key, should change to your own and keep security;用于cookie签名密钥，应更改为您自己的并保持安全
   config.keys = appInfo.name + '_1607653345197_6388';
+  config.projectName = {
+    AppName: appInfo.name,
+  };
   // add your middleware config here;在此处添加中间件配置
-  config.middleware = [];
+  config.middleware = [ 'jwtAuth', 'userAuth' ];
   // add your user config here;在此处添加您的用户配置
   const userConfig = {
-    // myAppName: 'egg',
+    myAppName: 'tutor',
   };
   // csrf config csrf配置
   config.security = {
@@ -44,12 +47,26 @@ module.exports = appInfo => {
       agent: false,
     },
   };
+  config.cluster = {
+    listen: {
+      port: 7001,
+      hostname: '127.0.0.1',
+    },
+  };
   config.paginatorConfig = {
     pageSize: 10,
   };
   config.uploadDir = 'app/public/static/upload';
   config.jwt = {
     secret: 'nPp1rYeZhq2HNfD3AO}I0aGV+Z^5/-LM~Wu7S&ejxKlJwiE4Fve+B8dgCXtQyUbs9oR6cT*k',
+  };
+  config.jwtAuth = {
+    enable: true,
+    ignore: [ '/business', '/admin/login', '/public' ],
+  };
+  config.userAuth = {
+    enable: true,
+    ignore: [ '/admin', '/business/user/login', '/public' ],
   };
   return {
     ...config,
