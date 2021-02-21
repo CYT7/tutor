5c1ca0f301aa48e2441beba69b610eb7518b3857
CYT7 <1490930847@qq.com>
2021-02-21 10:39:05 +0800

    完成轮播图创建功能

--- app/controller/admin/banner.js
+++ app/controller/admin/banner.js
@@ -13,37 +13,21 @@ class BannerController extends Controller {
     const { ctx } = this;
     const parts = ctx.multipart({ autoFields: true });
     const params = parts.field;
-    let files = {};
     let stream;
+    let dir = null;
     while ((stream = await parts()) != null) {
       if (!stream.filename) { break; }
-      const fieldname = stream.fieldname; // file表单的名字
-      const dir = await this.service.banner.adminBannerAdd(stream.filename);// 上传图片的目录
+      dir = await this.service.banner.saveCarousel(stream.filename);// 上传图片的目录
       const target = dir.uploadDir;
       const writeStream = fs.createWriteStream(target);
       await pump(stream, writeStream);
-      files = Object.assign(files, { [fieldname]: dir.saveDir });
-      if (!params.sequence) {
-        ctx.body = { code: -2, msg: '参数异常' };
-        return;
-      }
-      console.log('sequence' + params.sequence);
-      const check = await ctx.model.Banner.findOne({ sequence: params.sequence });
-      if (check) {
-        ctx.body = { code: 404601, msg: `序号：${params.sequence} 已存在，请更换其他序号或者删除原有的` };
-        return;
-      }
-      const newBanner = new ctx.model.Banner({
-        sequence: params.sequence,
-        carousel: dir.saveDir,
-        createTime: Math.round(new Date() / 1000),
-      });
-      newBanner.save();
     }
-    if (Object.keys(files).length > 0) {
-      ctx.body = { code: 0, msg: '添加轮播图成功' };
+    const res = await ctx.service.banner.adminBannerAdd(params, dir.saveDir);
+    if (res) {
+      ctx.status = 201;
+      ctx.body = { code: res[0], msg: res[1], token: res[2], exp: res[3] };
     } else {
-      ctx.body = { code: 404601, msg: '添加轮播图失败' };
+      ctx.body = { code: res[0], msg: res[1] };
     }
   }
 }
--- app/service/banner.js
+++ app/service/banner.js
@@ -13,7 +13,7 @@ const mkdirp = require('mkdirp');
 
 class BannerService extends Service {
   // 上传头像
-  async adminBannerAdd(filename) {
+  async saveCarousel(filename) {
     const { ctx, app } = this;
     const results = jwt(app, ctx.request.header.authorization);
     if (results[0]) { return [ -3, '请求失败' ]; }
@@ -24,6 +24,24 @@ class BannerService extends Service {
     const uploadDir = path.join(dir, date + path.extname(filename));
     const saveDir = this.ctx.origin + uploadDir.slice(3).replace(/\\/g, '/');
     return { uploadDir, saveDir };
+  }
+
+  async adminBannerAdd(params, Carousel) {
+    const { ctx, app } = this;
+    const results = jwt(app, ctx.request.header.authorization);
+    if (results[0]) { return [ -3, '请求失败' ]; }
+    if (!params.sequence) { return [ -2, '参数异常' ]; }
+    const check = await ctx.model.Banner.findOne({ sequence: params.sequence });
+    if (check) {
+      return [ 404601, `序号：${params.sequence} 已存在，请更换其他序号或者删除原有的` ];
+    }
+    const newBanner = new ctx.model.Banner({
+      sequence: params.sequence,
+      carousel: Carousel,
+      createTime: Math.round(new Date() / 1000),
+    });
+    newBanner.save();
+    return [ 0, '添加轮播图成功', results[1], results[2] ];
   }
 }
 module.exports = BannerService;
